f410935f24fc78ea16d4ecf5ac945b0465a0e630
hesiodepro <76748620+hesiodepro@users.noreply.github.com>
2025-02-15 19:32:22 +0300

    Update Chapter1_Summation.java

--- Chapter1_Summation.java
+++ Chapter1_Summation.java
@@ -3,7 +3,7 @@ public class Chapter1_Summation {
         
         double sum = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9;
         
-        // Display the results
-        System.out.println("The result is " + sum);
+        // Display the sum
+        System.out.println("The sum is " + sum);
     }
 }
