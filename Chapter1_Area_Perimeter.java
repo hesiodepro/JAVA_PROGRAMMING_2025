c200cf09222bef7553c59c27c8aaa3bc5d31cf90
hesiodepro <76748620+hesiodepro@users.noreply.github.com>
2025-02-15 19:35:48 +0300

    Create Chapter1_Area_Perimeter.java

---
+++ Chapter1_Area_Perimeter.java
@@ -0,0 +1,12 @@
+public class Chapter1_Area_Perimeter {
+    public static void main(String[] args) {
+        
+        double pi = 3.14159;
+        double radius = 5.5;
+        double area = radius * radius * pi;
+        double perimeter = 2 * radius * pi;
+        
+        // Display the output
+        System.out.println("The Area is " + area + " And the Perimeter is " + perimeter);
+    }
+}
