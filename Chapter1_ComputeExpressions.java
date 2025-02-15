d06f6e579af1346d09d6e8b2bc9df9c43264c595
hesiodepro <76748620+hesiodepro@users.noreply.github.com>
2025-02-15 19:15:58 +0300

    Create Chapter1_ComputeExpressions.java

---
+++ Chapter1_ComputeExpressions.java
@@ -0,0 +1,9 @@
+public class Chapter1_ComputeExpressions {
+    public static void main(String[] args) {
+        
+        double result = (9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5);
+        
+        // Display the results
+        System.out.println("The result is " + result);
+    }
+}
