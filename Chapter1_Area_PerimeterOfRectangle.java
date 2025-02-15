a48f7f52ab66aca762e3f5d3f3685e0af7ee6cc6
hesiodepro <76748620+hesiodepro@users.noreply.github.com>
2025-02-15 19:40:56 +0300

    Create Chapter1_Area_PerimeterOfRectangle.java

---
+++ Chapter1_Area_PerimeterOfRectangle.java
@@ -0,0 +1,12 @@
+public class Chapter1_Area_PerimeterOfRectangle {
+    public static void main(String[] args) {
+        
+        double width = 4.5;
+        double height = 7.9;
+        double area = width * height;
+        double perimeter = 2 * (width + height);
+        
+        // Display the output
+        System.out.println("The Area  of a rectangle is " + area + " And the Perimeter is " + perimeter);
+    }
+}
