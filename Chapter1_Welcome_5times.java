2412ea8c68bb5b9681334d6c3540e83cd5455245
hesiodepro <76748620+hesiodepro@users.noreply.github.com>
2025-02-15 18:34:25 +0300

    Create Chapter1_Welcome_5times.java

---
+++ Chapter1_Welcome_5times.java
@@ -0,0 +1,11 @@
+public class Chapter1_Welcome_5times {
+    public static void main (String[] args){
+        
+        System.out.println("Welcome to java");
+        System.out.println("Welcome to java");
+        System.out.println("Welcome to java");
+        System.out.println("Welcome to java");
+        System.out.println("Welcome to java");
+        
+    }
+}
