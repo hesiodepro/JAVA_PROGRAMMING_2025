f99c1175ae7319d3ebde0f95906ea3e4834c7add
hesiodepro <76748620+hesiodepro@users.noreply.github.com>
2025-02-15 18:05:03 +0300

    Create Welcome.java

---
+++ Welcome.java
@@ -0,0 +1,7 @@
+public class Welcome {
+    public static void main (String[] args){
+        //Display message Welcome ton java! on the console
+        System.out.println("Welcome to java!");
+    }
+    
+}
