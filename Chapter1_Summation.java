11680f6887e575ace3d7c1bdf023132407bf929f
hesiodepro <76748620+hesiodepro@users.noreply.github.com>
2025-02-15 19:18:50 +0300

    Create Chapter1_Summation.java

---
+++ Chapter1_Summation.java
@@ -0,0 +1,9 @@
+public class Chapter1_Summation {
+    public static void main(String[] args) {
+        
+        double sum = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9;
+        
+        // Display the results
+        System.out.println("The result is " + sum);
+    }
+}
