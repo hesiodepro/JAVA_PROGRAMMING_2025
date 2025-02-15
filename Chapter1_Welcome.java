eda6f1f2579e14f8c6ea5d92dfc45d86be13d0c1
hesiodepro <76748620+hesiodepro@users.noreply.github.com>
2025-02-15 18:31:46 +0300

    Create Chapter1_Welcome.java

---
+++ Chapter1_Welcome.java
@@ -0,0 +1,10 @@
+public class Chapter1_Welcome {
+    public static void main (String[] args){
+        
+        System.out.println("Welcome to java");
+        System.out.println("Welcome to computer science");
+        System.out.println("Programming is fun");
+      
+    }
+}
+  
