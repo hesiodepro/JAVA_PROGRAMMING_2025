23b818655c95e1c4b35d51aee80e97fdd9cffb31
hesiodepro <76748620+hesiodepro@users.noreply.github.com>
2025-02-21 17:17:50 +0300

    Create Chapter3_NumberOfDaysInMonth.java

---
+++ Chapter3/Chapter3_NumberOfDaysInMonth.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class  Chapter3_NumberOfDaysInMonth{
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the month (1-12): ");
+        int month = scanner.nextInt();
+        System.out.print("Enter the year: ");
+        int year = scanner.nextInt();
+
+        int days = 0;
+
+        switch (month) {
+            case 1:
+            case 3: 
+            case 5: 
+            case 7:
+            case 8: 
+            case 10:
+            case 12:
+                days = 31;
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                days = 30;
+                break;
+            case 2: 
+          
+                if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+                    days = 29;
+                } else {
+                    days = 28;
+                }
+                break;
+            default:
+                System.out.println("Invalid month!");
+                return;
+        }
+
+        System.out.println("Month " + month + " in the year " + year + " has " + days + " days.");
+
+    }
+}
