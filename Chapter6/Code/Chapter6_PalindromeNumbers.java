3e34c889cf55fb15110c47131ff598d839880bbd
hesiodepro <76748620+hesiodepro@users.noreply.github.com>
2025-03-01 08:25:17 +0300

    Create Chapter6_PalindromeNumbers.java

---
+++ Chapter6/Code/Chapter6_PalindromeNumbers.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Chapter6_PalindromeInteger {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int number = input.nextInt();
+        
+        if (isPalindrome(number)) {
+            System.out.println(number + " is a palindrome.");
+        } else {
+            System.out.println(number + " is not a palindrome.");
+        }
+    }
+
+    public static int reverse(int number) {
+        int reversed = 0;
+        while (number != 0) {
+            reversed = reversed * 10 + number % 10;
+            number /= 10;
+        }
+        return reversed;
+    }
+
+    public static boolean isPalindrome(int number) {
+        return number == reverse(number);
+    }
+}
