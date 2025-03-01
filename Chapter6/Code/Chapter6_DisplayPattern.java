41bff574ffb61087d6eb6d3f4995c6afe0369c43
hesiodepro <76748620+hesiodepro@users.noreply.github.com>
2025-03-01 08:34:03 +0300

    Create Chapter6_DisplayPattern.java

---
+++ Chapter6/Code/Chapter6_DisplayPattern.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Chapter6_DisplayPattern {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = input.nextInt();
+        displayPattern(n);
+    }
+
+    public static void displayPattern(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
