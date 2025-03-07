39b802f45d9c53905129c6669332f5bb20f08da2
hesiodepro <76748620+hesiodepro@users.noreply.github.com>
2025-03-07 09:35:08 +0300

    Create Chapter7_SmallestElement.java

---
+++ Chapter7/Codes/Chapter7_SmallestElement.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Chapter7_SmallestElement {
+    public static double min(double[] array) {
+        double min = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        return min;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double[] numbers = new double[10];
+
+        System.out.print("Enter 10 numbers: ");
+        for (int i = 0; i < 10; i++) {
+            numbers[i] = input.nextDouble();
+        }
+
+        System.out.println("The minimum number is " + min(numbers));
+    }
+}
