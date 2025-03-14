af8a149a26cb4300ba3559265e7d2be27253ef10
hesiodepro <76748620+hesiodepro@users.noreply.github.com>
2025-03-14 19:56:31 +0300

    Create Chapter9_Random.java

---
+++ Chapter 9/Code/Chapter9_Random.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+
+public class Chapter9_Random {
+    public static void main(String[] args) {
+        Random random = new Random(1000);
+        
+        for (int i = 0; i < 50; i++) {
+            System.out.print(random.nextInt(100) + " ");
+        }
+    }
+}
