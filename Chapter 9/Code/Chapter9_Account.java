fdfe1cc59087a74c015874251fce9e178d9bdbfe
hesiodepro <76748620+hesiodepro@users.noreply.github.com>
2025-03-14 20:10:27 +0300

    Create Chapter9_Account.java

---
+++ Chapter 9/Code/Chapter9_Account.java
@@ -0,0 +1,82 @@
+import java.util.Date;
+
+class Account {
+    private int id;
+    private double balance;
+    private static double annualInterestRate;
+    private Date dateCreated;
+
+    public Account() {
+        this.id = 0;
+        this.balance = 0;
+        this.dateCreated = new Date();
+    }
+
+    public Account(int id, double balance) {
+        this.id = id;
+        this.balance = balance;
+        this.dateCreated = new Date();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public static double getAnnualInterestRate() {
+        return annualInterestRate;
+    }
+
+    public static void setAnnualInterestRate(double rate) {
+        annualInterestRate = rate;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public double getMonthlyInterestRate() {
+        return annualInterestRate / 12;
+    }
+
+    public double getMonthlyInterest() {
+        return balance * (getMonthlyInterestRate() / 100);
+    }
+
+    public void withdraw(double amount) {
+        if (amount <= balance) {
+            balance -= amount;
+        } else {
+            System.out.println("Insufficient funds.");
+        }
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+}
+
+public class Chapter9_Account {
+    public static void main(String[] args) {
+        Account account = new Account(1122, 20000);
+        Account.setAnnualInterestRate(4.5);
+
+        account.withdraw(2500);
+        account.deposit(3000);
+
+        System.out.println("Balance: $" + account.getBalance());
+        System.out.println("Monthly Interest: $" + account.getMonthlyInterest());
+        System.out.println("Date Created: " + account.getDateCreated());
+    }
+}
