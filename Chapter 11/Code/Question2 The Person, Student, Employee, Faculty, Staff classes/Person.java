b11d25bcee6b807ee7a82b61911e51e3ccac6ad1
hesiodepro <76748620+hesiodepro@users.noreply.github.com>
2025-03-21 15:52:18 +0300

    Create Person.java

---
+++ Chapter 11/Code/Question2 The Person, Student, Employee, Faculty, Staff classes/Person.java
@@ -0,0 +1,17 @@
+public class Person {
+    private String name;
+    private String address;
+    private String phoneNumber;
+    private String emailAddress;
+
+    public Person(String name, String address, String phoneNumber, String emailAddress) {
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.emailAddress = emailAddress;
+    }
+
+    public String toString() {
+        return this.getClass().getSimpleName() + ": " + name;
+    }
+}
