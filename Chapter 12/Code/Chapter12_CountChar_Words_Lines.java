7075c041235bdf955dafde134a1e00df930ca2a2
hesiodepro <76748620+hesiodepro@users.noreply.github.com>
2025-03-21 18:13:42 +0300

    Create Chapter12_CountChar_Words_Lines.java

---
+++ Chapter 12/Code/Chapter12_CountChar_Words_Lines.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Chapter12_CountChar_Words_Lines{
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Usage: java CountFileStatistics <filename>");
+            return;
+        }
+
+        String filename = args[0];
+        
+        int numOfLines = 0;
+        int numOfWords = 0;
+        int numOfCharacters = 0;
+
+        try {
+            File file = new File(filename);
+            Scanner fileScanner = new Scanner(file);
+
+            while (fileScanner.hasNextLine()) {
+                String line = fileScanner.nextLine();
+                numOfLines++;
+                numOfCharacters += line.length();
+
+                // Split line into words using whitespace as a delimiter
+                String[] words = line.split("\\s+");
+                numOfWords += words.length;
+            }
+            fileScanner.close();
+
+            System.out.println("Number of lines: " + numOfLines);
+            System.out.println("Number of words: " + numOfWords);
+            System.out.println("Number of characters: " + numOfCharacters);
+
+        } catch (IOException e) {
+            System.out.println("An error occurred while reading the file: " + e.getMessage());
+        }
+    }
+}
