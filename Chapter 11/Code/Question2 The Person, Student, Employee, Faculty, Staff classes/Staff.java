c9df4d8453784524ae1eaa8613f60caf552a6c07
hesiodepro <76748620+hesiodepro@users.noreply.github.com>
2025-03-21 15:56:59 +0300

    Create Staff.java

---
+++ Chapter 11/Code/Question2 The Person, Student, Employee, Faculty, Staff classes/Staff.java
@@ -0,0 +1,12 @@
+public class Staff extends Employee {
+    private String title;
+
+    public Staff(String name, String address, String phoneNumber, String emailAddress, String office, double salary, MyDate dateHired, String title) {
+        super(name, address, phoneNumber, emailAddress, office, salary, dateHired);
+        this.title = title;
+    }
+
+    public String toString() {
+        return super.toString() + ", Title: " + title;
+    }
+}
