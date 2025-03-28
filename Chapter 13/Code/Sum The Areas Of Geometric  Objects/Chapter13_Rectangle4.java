b4dd30c81b4c49822c182538cae0f663793deaf5
hesiodepro <76748620+hesiodepro@users.noreply.github.com>
2025-03-28 21:35:16 +0300

    Create Chapter13_Rectangle4.java

---
+++ Chapter 13/Code/Sum The Areas Of Geometric  Objects/Chapter13_Rectangle4.java
@@ -0,0 +1,30 @@
+public class Chapter13_Rectangle4 extends Chapter13_GeometricObjects7 {
+    private double width;
+    private double height;
+
+    public Chapter13_Rectangle4(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    @Override
+    public double getArea() {
+        return width * height;  
+    }
+}
